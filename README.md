5b62d4331474b4d9a3d3983afa36ff6d0093638a
Carl <ruiqin.shen@outlook.com>
2018-05-25 16:14:18 +0800

    补充文档说明，implementation代替compile

--- README.md
+++ README.md
@@ -2,13 +2,12 @@
 ##集成步骤
 1） **build.gradle**添加</br>
 ```
- compile 'com.github.goEcar:EcarBugly:1.0.1'
+ compile 'com.github.goEcar:EcarBugly:1.1.0'
 ```
 2）**appliction的oncreate**添加</br>
 ```
-   BuglyUtil.init(this, BuildConfig.buglyId, false); // appid初始化
+   BuglyUtil.init(this, BuildConfig.buglyId, false, BuildConfig.VERSION_NAME);// appid初始化
                                                      //BuildConfig.buglyId: 当前appid  false 正式环境  true 测试环境
-   BuglyUtil.setVersion(this,BuildConfig.VERSION_NAME); //当前APP版本号
 ```
 3） **混淆**(不报错可不加)</br>
 ```
--- app/src/main/java/bugly/ecar/com/ecarbugly/App.java
+++ app/src/main/java/bugly/ecar/com/ecarbugly/App.java
@@ -11,14 +11,14 @@ import bugly.ecar.com.ecarbuglylib.util.BuglyUtil;
  * 版权所有：深圳市亿车科技有限公司
  *************************************/
 
-public class app extends Application {
+public class App extends Application {
 
 
     @Override
     public void onCreate() {
         super.onCreate();
 
-        BuglyUtil.init(this, BuildConfig.buglyId, BuildConfig.IS_DEBUG, BuildConfig.VERSION_NAME);      //初始化（需要tinker）
+        BuglyUtil.init(this, BuildConfig.buglyId, false, BuildConfig.VERSION_NAME);      //初始化（需要tinker）
 //      BuglyUtil.init(this, BuildConfig.buglyId, BuildConfig.IS_DEBUG,BuildConfig.VERSION_NAME,false);        //初始化（不需要tinker）
 
 
